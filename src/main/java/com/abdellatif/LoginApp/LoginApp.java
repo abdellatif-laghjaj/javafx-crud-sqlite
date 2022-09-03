245ff0ee039b5a632d4010a05d43b8e97c1f7e4d
Abdellatif Laghjaj <abdellatif.laghjaj@edu.uiz.ac.ma>
2022-09-03 14:14:11 +0100

    update view

--- src/main/java/com/abdellatif/LoginApp/LoginApp.java
+++ src/main/java/com/abdellatif/LoginApp/LoginApp.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class LoginApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApp.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApp.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
